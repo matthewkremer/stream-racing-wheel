9d3f9d906410e2462643f0a610e4cd27c8ef342b
Matt Kremer <mkremer90@gmail.com>
2021-07-01 11:15:06 -0500

    Add build directory

---
+++ build/precache-manifest.9da1a4be0bba223bb27993e43b379268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbeb5758e719d3bcc6990e9daaa906d",
+    "url": "/stream-racing-wheel/index.html"
+  },
+  {
+    "revision": "f7f113e232b50bcab152",
+    "url": "/stream-racing-wheel/static/css/main.b44067e2.chunk.css"
+  },
+  {
+    "revision": "58a2fe0432c59d489235",
+    "url": "/stream-racing-wheel/static/js/2.8c0f546b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/stream-racing-wheel/static/js/2.8c0f546b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f113e232b50bcab152",
+    "url": "/stream-racing-wheel/static/js/main.a2f635b8.chunk.js"
+  },
+  {
+    "revision": "bd94bbd584e5d9cae908",
+    "url": "/stream-racing-wheel/static/js/runtime-main.2ecfeb33.js"
+  }
+]);
